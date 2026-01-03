82ee7e2b5c9a05c3f24a7b0fab0d62519a01fea7
Yara <yarak7047@gmail.com>
2026-01-03 15:15:02 +0400

    change somethings to test

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreen),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter My Project Home Page'),
     );
   }
 }
@@ -64,6 +64,12 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+    });
+  }
+
+  void _decrecmentCounter(){
+    setState(() {
+      _counter--;
     });
   }
 
@@ -104,17 +110,37 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Hello from our app'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(child: Container(
+                  padding: EdgeInsets.only(bottom: 15),
+                  color: Colors.orangeAccent,
+                  child: IconButton(
+                      onPressed: _incrementCounter
+                      , icon: Icon(Icons.add),),
+                )),
+                Expanded(child: Container(
+                  padding: EdgeInsets.only(bottom: 15),
+                  color: Colors.red,
+                  child: IconButton(
+                    onPressed: _decrecmentCounter,
+                      icon:Icon(Icons.minimize_outlined)),
+                )),
+              ],
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Decreasing',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
