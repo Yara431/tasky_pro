13f6f14f0a9c757a9ddc7be39994e11392950fa0
Yara <yarak7047@gmail.com>
2026-01-06 17:21:54 +0400

    make the app structure and building on boarding page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tasky_pro/authentication/view/onboarding_view.dart';
-import 'package:tasky_pro/authentication/view/sign_in_view.dart';
 
 void main() {
   runApp( MyApp());
